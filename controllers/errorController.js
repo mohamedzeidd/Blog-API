d66a0829647ddb14a26f97f6db5d6c1a03cfc9c5
mohamedzeidd <117725555+mohamedzeidd@users.noreply.github.com>
2025-02-19 18:58:24 +0200

    Update errorController.js

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -74,6 +74,7 @@ const sendErrorProd = (err, req, res) => {
 };
 
 module.exports = (err, req, res, next) => {
+  console.log(err);
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
 
